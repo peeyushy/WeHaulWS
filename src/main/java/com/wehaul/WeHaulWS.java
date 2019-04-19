e3927d3a9a91698591cdd7e31016480d409fd95f
peeyushy <31182352+peeyushy@users.noreply.github.com>
2019-04-19 05:26:42 +0100

    tomcat container related changes
    tomcat container related changes

--- src/main/java/com/wehaul/WeHaulWS.java
+++ src/main/java/com/wehaul/WeHaulWS.java
@@ -2,22 +2,30 @@ package com.wehaul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ConcurrentTaskScheduler;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableScheduling
-public class WeHaulWS {
+public class WeHaulWS extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WeHaulWS.class, args);
 	}
-	
+
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
-	    return new BCryptPasswordEncoder();
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public TaskScheduler taskScheduler() {
+		return new ConcurrentTaskScheduler();
 	}
 }
